39740170ef0a6cab377af946f2a2e25dab3ff287
RoselineDC <roseline.danga@gmail.com>
2025-05-24 17:52:34 +0200

    modified by Roseline on 2025-05-24 17:52:33

--- tenderscraper/tenderscraper/spiders/transnetscraper.py
+++ tenderscraper/tenderscraper/spiders/transnetscraper.py
@@ -4,7 +4,7 @@ import scrapy
 class TransnetscraperSpider(scrapy.Spider):
     name = "transnetscraper"
     allowed_domains = ["transnetetenders.azurewebsites.net"]
-    start_urls = ["https://transnetetenders.azurewebsites.net/Home/AdvertisedTenders"]
+    start_urls = ["https://transnetetenders.azurewebsites.net/Home/GetAdvertisedTenders"]
 
     def parse(self, response):
         pass
