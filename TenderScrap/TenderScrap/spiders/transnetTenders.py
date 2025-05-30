b8becfc7b8541416e2acf12d5332d97ce3f72340
RoselineDC <roseline.danga@gmail.com>
2025-05-30 16:38:26 +0200

    modified by Roseline on 2025-05-30 16:38:26

--- TenderScrap/TenderScrap/spiders/transnetTenders.py
+++ TenderScrap/TenderScrap/spiders/transnetTenders.py
@@ -31,6 +31,7 @@ class TransnettendersSpider(scrapy.Spider):
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
         "x-requested-with": "XMLHttpRequest"
     }
+        
 
 
     def parse(self, response):
