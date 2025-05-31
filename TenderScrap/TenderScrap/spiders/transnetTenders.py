e0733d43b902afa4ba9bd1634ddd0cea3885ed30
RoselineDC <roseline.danga@gmail.com>
2025-05-31 12:44:47 +0200

    modified by Roseline on 2025-05-31 12:44:46

--- TenderScrap/TenderScrap/spiders/transnetTenders.py
+++ TenderScrap/TenderScrap/spiders/transnetTenders.py
@@ -36,6 +36,7 @@ class TransnetTendersSpider(scrapy.Spider):
                     "Briefing Date": tender.get("briefingDate"),
                     "Location": tender.get("locationOfService"),
                     "Tender Document URL": tender.get("attachment"),
+                    ""
                     "Contact Person": tender.get("contactPersonName"),
                     "Contact Email": tender.get("contactPersonEmailAddress"),
                 }
