f46e2db3ba31e8ad8ba2bb200d67a1509dfce69d
RoselineDC <roseline.danga@gmail.com>
2025-05-31 12:41:38 +0200

    modified by Roseline on 2025-05-31 12:41:38

--- TenderScrap/TenderScrap/spiders/transnetTenders.py
+++ TenderScrap/TenderScrap/spiders/transnetTenders.py
@@ -33,6 +33,7 @@ class TransnetTendersSpider(scrapy.Spider):
                     "Description": tender.get("descriptionOfTender"),
                      "Published Date": tender.get("publishedDate"),
                     "Closing Date": tender.get("closingDate"),
+                    "B"
                     "Location": tender.get("locationOfService"),
                     "Published Date": tender.get("publishedDate"),
                     "CIDB Grade": tender.get("cidbGrade"), 
