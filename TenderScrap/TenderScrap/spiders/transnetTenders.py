b554c2cee05b06ea7b6508647c709390b605975a
RoselineDC <roseline.danga@gmail.com>
2025-05-31 13:27:23 +0200

    modified by Roseline on 2025-05-31 13:27:22

--- TenderScrap/TenderScrap/spiders/transnetTenders.py
+++ TenderScrap/TenderScrap/spiders/transnetTenders.py
@@ -28,6 +28,8 @@ class TransnetTendersSpider(scrapy.Spider):
             data = json.loads(response.text)
             tenders = data.get("result", [])
             for tender in tenders:
+                row_key = tender.get("rowKey")
+        tender_url = f"https://transnetetenders.azurewebsites.net/Home/TenderDetails?Id={row_key}"
                 yield {
                     "Tender Number": tender.get("tenderNumber"),
                     "Description": tender.get("descriptionOfTender"),
