2f77bc885865a62f88a9b11370bf9391e90febdd
RoselineDC <roseline.danga@gmail.com>
2025-06-03 22:33:25 +0200

    modified by Roseline on 2025-06-03 22:33:24

--- TenderScrap/TenderScrap/spiders/pdate_tenders.py
+++ TenderScrap/TenderScrap/spiders/pdate_tenders.py
@@ -5,4 +5,5 @@ def clean_and_sort_csv(file_path='advert.csv'):
     df = pd.read_csv(file_path)
 
     # convert date columns to datetime 
-    for col in ["published Date", "Closing Date"]
+    for col in ["published Date", "Closing Date"]:
+        df[col] = pd.to_datetime(df[col], errors='coerce')
