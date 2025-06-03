3789551852861c5579a58f2d765a7221e6544e8c
RoselineDC <roseline.danga@gmail.com>
2025-06-03 22:53:34 +0200

    modified by Roseline on 2025-06-03 22:53:34

--- TenderScrap/TenderScrap/spiders/pdate_tenders.py
+++ TenderScrap/TenderScrap/spiders/pdate_tenders.py
@@ -5,7 +5,7 @@ def clean_and_sort_csv(file_path='advert.csv'):
     df = pd.read_csv(file_path)
 
     # convert date columns to datetime 
-    for col in ["Published Dateae", "Closing Date"]:
+    for col in ["Published Date", "Closing Date"]:
         df[col] = pd.to_datetime(df[col], errors='coerce')
 
     # drop rows with NaT in 'published Date' or 'Closing Date'
