50a31c61734997f8d253170db51879e28d328ce5
RoselineDC <roseline.danga@gmail.com>
2025-06-03 22:53:10 +0200

    modified by Roseline on 2025-06-03 22:53:09

--- TenderScrap/TenderScrap/spiders/pdate_tenders.py
+++ TenderScrap/TenderScrap/spiders/pdate_tenders.py
@@ -5,14 +5,14 @@ def clean_and_sort_csv(file_path='advert.csv'):
     df = pd.read_csv(file_path)
 
     # convert date columns to datetime 
-    for col in ["Published Date Date", "Closing Date"]:
+    for col in ["Published Dateate", "Closing Date"]:
         df[col] = pd.to_datetime(df[col], errors='coerce')
 
     # drop rows with NaT in 'published Date' or 'Closing Date'
     df = df.drop_duplicates(subset=["Tender Number"], keep='last')
 
     #sort by published date decending
-    df = df.sort_values(by="published Date", ascending=False)
+    df = df.sort_values(by="Published Date", ascending=False)
 
     # save cleaned and sorted DataFrame to a new CSV file
     df.to_csv(file_path, index=False)
