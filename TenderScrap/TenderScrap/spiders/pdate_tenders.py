d796d4236aa8cbef3416ab28848c8dd5650eaf1e
RoselineDC <roseline.danga@gmail.com>
2025-06-03 22:35:50 +0200

    modified by Roseline on 2025-06-03 22:35:49

--- TenderScrap/TenderScrap/spiders/pdate_tenders.py
+++ TenderScrap/TenderScrap/spiders/pdate_tenders.py
@@ -11,4 +11,4 @@ def clean_and_sort_csv(file_path='advert.csv'):
     # drop rows with NaT in 'published Date' or 'Closing Date'
     df = df.drop_duplicates(subset=["Tender Number"], keep='last')
 
-    #sort by publishe
+    #sort by published date decending
