4b160332205dff2bb5de9e47115cf9947fae6cab
RoselineDC <roseline.danga@gmail.com>
2025-06-10 15:45:46 +0200

    modified by Roseline on 2025-06-10 15:45:46

--- Dashboard/tenderdashboard/app/Navbar.tsx
+++ Dashboard/tenderdashboard/app/Navbar.tsx
@@ -12,32 +12,4 @@ const handleDrawerToggle = () => {
     setIsDrawerOpen(!isDrawerOpen);
     };
 // create Navbar component
-const Navbar = () => {
-    return (
-        <nav className="bg-gray-800 text-white p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <Link href="/" className="text-xl font-bold">
-                    Tender Dashboard
-                </Link>
-                <button
-                    onClick={handleDrawerToggle}
-                    className="text-white md:hidden"
-                >
-                    <FontAwesomeIcon icon={isDrawerOpen ? faXmark : faBars} />
-                </button>
-            </div>
-            {isDrawerOpen && (
-                <div className="md:hidden bg-gray-700 p-4">
-                    <ul>
-                        <li className="py-2">
-                            <Link href="/tenders">Tenders</Link>
-                        </li>
-                        <li className="py-2">
-                            <Link href="/about">About</Link>
-                        </li>
-                    </ul>
-                </div>
-            )}
-        </nav>
-    );
-};
+const 
