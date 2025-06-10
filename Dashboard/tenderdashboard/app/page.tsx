24600b20f19d70c3a2851d96833fb2b87ddc4214
RoselineDC <roseline.danga@gmail.com>
2025-06-10 16:02:15 +0200

    modified by Roseline on 2025-06-10 16:02:15

--- Dashboard/tenderdashboard/app/page.tsx
+++ Dashboard/tenderdashboard/app/page.tsx
@@ -1,4 +1,4 @@
-""
+""use client";
 import Image from "next/image";
 import Navbar from "./Navbar";
 import { use } from "react";
@@ -12,3 +12,4 @@ export default function Home() {
     </div>
     
   );
+}
