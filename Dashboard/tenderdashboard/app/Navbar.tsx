2c27a126c0faca619adeae52fa752b2f49ab30e9
RoselineDC <roseline.danga@gmail.com>
2025-06-10 15:48:57 +0200

    modified by Roseline on 2025-06-10 15:48:57

--- Dashboard/tenderdashboard/app/Navbar.tsx
+++ Dashboard/tenderdashboard/app/Navbar.tsx
@@ -1,29 +1,27 @@
-import Link from 'next/link';
-import React, {useState} from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
-
+import Link from "next/link";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 // create dropdown
- const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
- //handle drawer toggle
+//handle drawer toggle
 const handleDrawerToggle = () => {
-    setIsDrawerOpen(!isDrawerOpen);
-    };
+  setIsDrawerOpen(!isDrawerOpen);
+};
 // create Navbar component
 const Navbar = () => {
-    return (
-        <div>
-            <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
-                 <ul className="absolute right-0 flex flex-row space-x-6">
-      <li className="text-xl hover:text-blue-900 hover:font-semibold">
-        <Link href="/">Home</Link>
-      </li>
-      {/* Add other navigation links */}
-    </ul>
-                </nav>
-        </div>
-    )
-
-}
+  return (
+    <div>
+      <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
+        <ul className="absolute right-0 flex flex-row space-x-6">
+          <li className="text-xl hover:text-blue-900 hover:font-semibold">
+            <Link href="/">Home</Link>
+          </li>
+          {/* Add other navigation links */}
+        </ul>
+      </nav>
+    </div>
+  );
+};
