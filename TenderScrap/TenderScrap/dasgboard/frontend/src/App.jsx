c9005e6625fde3b865b7996a46501c38c45c4eed
RoselineDC <roseline.danga@gmail.com>
2025-06-10 23:18:36 +0200

    modified by Roseline on 2025-06-10 23:18:35

--- TenderScrap/TenderScrap/dasgboard/frontend/src/App.jsx
+++ TenderScrap/TenderScrap/dasgboard/frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TenderList from './TenderList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Ten
+      <TenderList />
     </>
   )
 }
