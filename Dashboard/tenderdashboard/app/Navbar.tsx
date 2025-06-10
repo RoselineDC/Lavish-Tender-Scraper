1d14d90fede28398fafe3d702fc280410642389d
RoselineDC <roseline.danga@gmail.com>
2025-06-10 15:50:33 +0200

    modified by Roseline on 2025-06-10 15:50:32

--- Dashboard/tenderdashboard/app/Navbar.tsx
+++ Dashboard/tenderdashboard/app/Navbar.tsx
@@ -31,7 +31,9 @@ const Navbar = () => {
           <li className="text-xl hover:text-blue-900 hover:font-semibold">
             <Link href="/">Home</Link>
           </li>
-          
+          <li className="text-xl hover:text-blue-900 hover:font-semibold">
+            <Link href="/">Home</Link>
+          </li>
           {/* Add other navigation links */}
         </ul>
       </nav>
