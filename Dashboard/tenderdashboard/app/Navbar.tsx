1ed0d0bb30f3f8b6f17e18bf3e4585db6426b91c
RoselineDC <roseline.danga@gmail.com>
2025-06-10 15:57:06 +0200

    modified by Roseline on 2025-06-10 15:57:06

--- Dashboard/tenderdashboard/app/Navbar.tsx
+++ Dashboard/tenderdashboard/app/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             <Link href="/">Tenders</Link>
           </li>
           <li className="text-xl hover:text-blue-900 hover:font-semibold">
-            <Link href="/"></Link>
+            <Link href="/">Transnet</Link>
           </li>
           <li className="text-xl hover:text-blue-900 hover:font-semibold">
             <Link href="/">Home</Link>
