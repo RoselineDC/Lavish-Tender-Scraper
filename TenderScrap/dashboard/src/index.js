9aa0fa36ba48c54b9f455d6b92c6a9120eb37ac3
RoselineDC <roseline.danga@gmail.com>
2025-06-11 23:15:08 +0200

    modified by Roseline on 2025-06-11 23:15:07

--- TenderScrap/dashboard/src/index.js
+++ TenderScrap/dashboard/src/index.js
@@ -4,15 +4,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
+ 
     <App />,
   document.getElementById('root')
 )
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
-}
-
-export default index
