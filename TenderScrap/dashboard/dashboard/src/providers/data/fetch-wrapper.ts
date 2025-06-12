854a85b71138dc8a48b46d612526a05ce676f894
RoselineDC <roseline.danga@gmail.com>
2025-06-12 21:53:04 +0200

    modified by Roseline on 2025-06-12 21:53:03

--- TenderScrap/dashboard/dashboard/src/providers/data/fetch-wrapper.ts
+++ TenderScrap/dashboard/dashboard/src/providers/data/fetch-wrapper.ts
@@ -13,7 +13,7 @@ const customFetch = async (url: string, options: RequestInit) => {
         headers: {
             ...headers,
             "Content-Type": "application/json",
-            Authorization: h ? `Bearer ${accessToken}` : "",
+            Authorization: headers ? `Bearer ${accessToken}` : "",
         },
     });
 
