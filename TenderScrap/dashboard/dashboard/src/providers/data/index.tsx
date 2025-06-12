56d4a35faf55091dc92d40dfa3a9603b47169478
RoselineDC <roseline.danga@gmail.com>
2025-06-12 21:46:23 +0200

    modified by Roseline on 2025-06-12 21:46:23

--- TenderScrap/dashboard/dashboard/src/providers/data/index.tsx
+++ TenderScrap/dashboard/dashboard/src/providers/data/index.tsx
@@ -7,7 +7,7 @@ export const client = new GraphQLClient( API_URL, {
         try{
 
         } catch (error) {
-           return Promise.reject(error);
+           return Promise.reject(error as);
         }
 
     }
