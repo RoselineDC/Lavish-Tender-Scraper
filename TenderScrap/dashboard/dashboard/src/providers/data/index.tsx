523c84d49313e32872ec5b3f4b3950cd6a071fd2
RoselineDC <roseline.danga@gmail.com>
2025-06-12 21:44:58 +0200

    modified by Roseline on 2025-06-12 21:44:58

--- TenderScrap/dashboard/dashboard/src/providers/data/index.tsx
+++ TenderScrap/dashboard/dashboard/src/providers/data/index.tsx
@@ -3,6 +3,7 @@ import { GraphQLClient } from 'graphql-request';
 export const API_URL = 'https://api.crm.refine.dev/';
 
 export const client = new GraphQLClient( API_URL, { 
-    fetch: (url: string, options: RequestInit) => 
-        {}
+    fetch: (url: string, options: RequestInit) => {
+        
+    }
 })
