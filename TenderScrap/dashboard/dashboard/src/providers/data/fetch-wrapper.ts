630c081cda67bd2b667d5c3f06258252e5b9039a
RoselineDC <roseline.danga@gmail.com>
2025-06-12 22:33:22 +0200

    modified by Roseline on 2025-06-12 22:33:22

--- TenderScrap/dashboard/dashboard/src/providers/data/fetch-wrapper.ts
+++ TenderScrap/dashboard/dashboard/src/providers/data/fetch-wrapper.ts
@@ -32,7 +32,7 @@ Error |null => {
     if("errors" in body){
         const errors = body.errors;
 
-        const messages = errors?>map
+        const messages = errors?>map666
     }
     return null;
 }
