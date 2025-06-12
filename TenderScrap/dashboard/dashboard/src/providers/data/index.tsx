0c8c5099de1b0387fa420b0a805db8a1861b1e07
RoselineDC <roseline.danga@gmail.com>
2025-06-12 21:44:23 +0200

    modified by Roseline on 2025-06-12 21:44:22

--- TenderScrap/dashboard/dashboard/src/providers/data/index.tsx
+++ TenderScrap/dashboard/dashboard/src/providers/data/index.tsx
@@ -1,5 +1,7 @@
 import { GraphQLClient } from 'graphql-request';
 
-export const API_URL = 'https://api.crm.refine.dev';
+export const API_URL = 'https://api.crm.refine.dev/';
 
-export const client = new GraphQLClient( API_URL, { })
+export const client = new GraphQLClient( API_URL, { 
+    
+})
