56ebb2122bd3d060f2c6f7a992ae689acc37f06a
RoselineDC <roseline.danga@gmail.com>
2025-06-12 21:59:38 +0200

    modified by Roseline on 2025-06-12 21:59:38

--- TenderScrap/dashboard/dashboard/src/providers/data/fetch-wrapper.ts
+++ TenderScrap/dashboard/dashboard/src/providers/data/fetch-wrapper.ts
@@ -20,4 +20,4 @@ const customFetch = async (url: string, options: RequestInit) => {
 
 }
 
-const getGraphErrors = (body: Record<"errors", GraphQLFormattedError[] | undefined>) => {
+const getGraphErrors = (body: Record<"errors", GraphQLFormattedError[] | undefined>) => {}
