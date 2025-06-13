eec905dc1b88ba16d90d2e55d132e3386287e2d0
RoselineDC <roseline.danga@gmail.com>
2025-06-13 15:50:56 +0200

    modified by Roseline on 2025-06-13 15:50:54

--- TenderScrap/dashboard/src/components/Sidebar/Sidebar.tsx
+++ TenderScrap/dashboard/src/components/Sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Sidebar = () => {
   return (
     <div>
-        <div className='overflow-y-scroll sticky '>
+        <div className='overflow-y-scroll sticky top-4 '>
             {/* main side content */}
 
         </div>
