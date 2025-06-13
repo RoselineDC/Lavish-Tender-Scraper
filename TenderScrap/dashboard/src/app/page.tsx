eeaddbb8a3555a73fb0374587efca9a73da86579
RoselineDC <roseline.danga@gmail.com>
2025-06-13 15:03:57 +0200

    modified by Roseline on 2025-06-13 15:03:57

--- TenderScrap/dashboard/src/app/page.tsx
+++ TenderScrap/dashboard/src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Dashboard } from "@/components/Dashboard/Dashboard";
 // import { Sidebar } from "@/components/Sidebar/Sidebar";
-import {Sidebsr}
+import {Sideb}
 export default function Home() {
   return (
     <main className="grid gap-4 p-4 grid-cols-[220px,_1fr]">
