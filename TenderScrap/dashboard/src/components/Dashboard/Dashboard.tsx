92aebfe921d0c80fb1296c137bf8b70232608828
RoselineDC <roseline.danga@gmail.com>
2025-06-13 15:47:08 +0200

    modified by Roseline on 2025-06-13 15:47:08

--- TenderScrap/dashboard/src/components/Dashboard/Dashboard.tsx
+++ TenderScrap/dashboard/src/components/Dashboard/Dashboard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='bg-grey rounded-lg shadow h-[200vh]'>Dashboard</div>
+    <div className='bg-white rounded-lg shadow h-[200vh]'>Dashboard</div>
   )
 }
 
