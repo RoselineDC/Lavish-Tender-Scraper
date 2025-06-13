b4c147c79b4e4830abc50c301b3236a76377325f
RoselineDC <roseline.danga@gmail.com>
2025-06-13 15:57:17 +0200

    modified by Roseline on 2025-06-13 15:57:16

--- TenderScrap/dashboard/src/components/Dashboard/Dashboard.tsx
+++ TenderScrap/dashboard/src/components/Dashboard/Dashboard.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Dashboard = () =>{
     return (
         <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]">
+            
         {/* main dashboard content */}
         </div>
     )
