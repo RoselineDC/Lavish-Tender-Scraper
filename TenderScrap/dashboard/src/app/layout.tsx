8a90638efd8ca66768ab0d4ae40037a78dcba39e
RoselineDC <roseline.danga@gmail.com>
2025-06-13 14:51:13 +0200

    modified by Roseline on 2025-06-13 14:51:12

--- TenderScrap/dashboard/src/app/layout.tsx
+++ TenderScrap/dashboard/src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} text-stone-950 bg-`}
+        className={`${inter.className} text-stone-950 bg-stone-1`}
       >
         {children}
       </body>
