ef481a882e4099fcc80a2be4a11f9a5be9c52598
RoselineDC <roseline.danga@gmail.com>
2025-06-13 15:56:21 +0200

    modified by Roseline on 2025-06-13 15:56:21

--- TenderScrap/dashboard/src/components/Dashboard/Dashboard.tsx
+++ TenderScrap/dashboard/src/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Dashboard = () =>{} {
-  return (
-   <div className="bg-white rounded-lg pb-4 shadow">
-  )
+const Dashboard = () =>{
+    return (
+        <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]">
+        {/* main dashboard content */}
+        </div>
+    )
 }
 
 export default Dashboard
