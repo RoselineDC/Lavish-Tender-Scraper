e4d8fa3980e38856bd301a43f4c1aa4dfafdf2b9
RoselineDC <roseline.danga@gmail.com>
2025-06-15 23:03:12 +0200

    modified by Roseline on 2025-06-15 23:03:12

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      
+      </Navbar>
+      <div className="flex items-center justify-center h-screen bg-gray-50">
     </main>
    
   );
