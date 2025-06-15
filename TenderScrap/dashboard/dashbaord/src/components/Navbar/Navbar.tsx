247f1630d085193f89ba56f290d98975c75ffc35
RoselineDC <roseline.danga@gmail.com>
2025-06-16 00:16:49 +0200

    modified by Roseline on 2025-06-16 00:16:49

--- TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
+++ TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ export default function UpNavBar() {
   return (
     <header className="bg-gray-500 px-4 py-2 flex justify-between items-center">
       {/* Left menu icon */}
-      <div  className="text-white text-xl">
+      <div  onClickclassName="text-white text-xl">
         <FaBars />
       </div>
 
