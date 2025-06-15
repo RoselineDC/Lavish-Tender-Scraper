7286332ecf63c95374e1d540641d277bd1d66bcb
RoselineDC <roseline.danga@gmail.com>
2025-06-16 00:12:05 +0200

    modified by Roseline on 2025-06-16 00:12:05

--- TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
+++ TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import { FaBars, FaEnvelope, FaBell, FaFlag, FaCog } from 'react-icons/fa';
 
 export default function UpNavBar() {
   return (
-    <header className="bg-gray-00 px-4 py-2 flex justify-between items-center">
+    <header className="bg-gray-500 px-4 py-2 flex justify-between items-center">
       {/* Left menu icon */}
       <div className="text-white text-xl">
         <FaBars />
