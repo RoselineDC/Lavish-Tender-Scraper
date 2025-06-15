86ab9098e8da9e91e2f2ca28bd58b4982e94ed4f
RoselineDC <roseline.danga@gmail.com>
2025-06-16 00:47:19 +0200

    modified by Roseline on 2025-06-16 00:47:18

--- TenderScrap/dashboard/dashbaord/src/app/layout.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <main className="max-w-10xl mx-auto">
-          <UpNavBar />
+        
           {children}
         </main>
       </body>
