fddcd5069e77761643e58d6a7a37574864fbc70e
RoselineDC <roseline.danga@gmail.com>
2025-06-15 23:03:19 +0200

    modified by Roseline on 2025-06-15 23:03:18

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       </Navbar>
-      <div className="flex items-center justify-center h-screen bg-gray-50">
+      
     </main>
    
   );
