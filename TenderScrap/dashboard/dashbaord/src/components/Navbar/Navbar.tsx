aa2d44f792e1b7c79717bc761f22139addfc0600
RoselineDC <roseline.danga@gmail.com>
2025-06-16 00:48:48 +0200

    modified by Roseline on 2025-06-16 00:48:47

--- TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
+++ TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ export default function UpNavBar({ toggleSidebar }: { toggleSidebar: () => void 
   const [showSidebar, setShowSidebar] = useState(false)
 
   return (
-    <DIV></DIV>
+    <div
     <header className="bg-gray-500 px-4 py-2 flex justify-between items-center">
       
       {/* Left menu icon */}
