ff9859e97dc243b9e062b37d29ffde932e580bf0
RoselineDC <roseline.danga@gmail.com>
2025-06-16 00:03:59 +0200

    modified by Roseline on 2025-06-16 00:03:57

--- TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
+++ TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
@@ -144,7 +144,14 @@ export default function UpNavBar() {
                 </MenuItem>
               </MenuItems>
             </Menu>
-            
+            <button
+        type="button"
+        className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+      >
+        <span className="absolute -inset-1.5" />
+        <span className="sr-only">View settings</span>
+        <Cog8ToothIcon className="size-6" aria-hidden="true" />
+      </button>
 
           </div>
         </div>
