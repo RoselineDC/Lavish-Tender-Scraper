ab73364afb46c10790a52efbc5660e4723706d0c
RoselineDC <roseline.danga@gmail.com>
2025-06-15 23:03:46 +0200

    modified by Roseline on 2025-06-15 23:03:45

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <Navbar />
-      
+      <Navbar>
+        {/* Add navbar content here or leave empty if not needed      
     </main>
    
   );
