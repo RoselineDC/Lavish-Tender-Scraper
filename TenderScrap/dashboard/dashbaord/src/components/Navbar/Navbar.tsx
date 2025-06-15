ffd1ad985abbf8c1f8745fe03e79000eed4cc988
RoselineDC <roseline.danga@gmail.com>
2025-06-15 22:59:01 +0200

    modified by Roseline on 2025-06-15 22:59:00

--- TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
+++ TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-exportconst Navbar = ({ children }: { children: React.ReactNode }) => (
+econst Navbar = ({ children }: { children: React.ReactNode }) => (
   <nav className="flex items-center justify-between px-4 py-2 bg-gray-100 border-b">{children}</nav>
 );
 
