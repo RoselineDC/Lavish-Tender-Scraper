2a2c3fbd4c8bed429ebcc3ad2235e161c2c62c37
RoselineDC <roseline.danga@gmail.com>
2025-06-16 01:00:19 +0200

    modified by Roseline on 2025-06-16 01:00:19

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
         </Sidebar>
       )}
 
-      <div className="fflex flex-col h-screen">
+      <div className="flex-1 flex flex-col h-screen">
         <UpNavBar toggleSidebar={() => setShowSidebar(!showSidebar)} />
         <main className="p-4">
           <h1 className="text-xl">Welcome to e-Tenders</h1>
