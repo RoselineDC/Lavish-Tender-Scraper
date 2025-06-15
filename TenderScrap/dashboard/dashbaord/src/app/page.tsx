f97dd8114bd6d90d4e78d71ea848cd4de15d5c22
RoselineDC <roseline.danga@gmail.com>
2025-06-16 00:37:59 +0200

    modified by Roseline on 2025-06-16 00:37:59

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 // Make sure the file exists at ../components/Navbar.tsx or ../components/Navbar/index.tsx
-import Navbarimport Sidebar from '../components/Sidebar'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/'
 
 export default function Home() {
   const [showSidebar, setShowSidebar] = useState(false)
@@ -11,4 +12,9 @@ export default function Home() {
       <div className="flex-1">
         <Navbar onToggle={() => setShowSidebar(!showSidebar)} />
         <main className="p-4">
-          <h2>Wel
+          <h2>Welcome to the homepage!</h2>
+        </main>
+      </div>
+    </div>
+  )
+}
