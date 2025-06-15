b391ee104e7dd83ebd40acdf6d0ede59b8b496b4
RoselineDC <roseline.danga@gmail.com>
2025-06-16 00:56:09 +0200

    modified by Roseline on 2025-06-16 00:56:08

--- TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
+++ TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
@@ -7,12 +7,12 @@ export default function UpNavBar({ toggleSidebar }: { toggleSidebar: () => void 
   const [showSidebar, setShowSidebar] = useState(false)
 
   return (
-    <><div className="bg-gray-400 px-4 py-2 flex items-center justify-center">
+    <><div className="bg-gray-500 px-4 py-2 flex items-center justify-center">
        <div className="text-white font-semibold text-lg">
         e<span className="font-normal">-Tenders</span>
       </div>
     </div>
-    <header className="bg-blue-400 px-4 py-2 flex justify-between items-center">
+    <header className="bg-blu-400 px-4 py-2 flex justify-between items-center">
       
       {/* Left menu icon */}
       <div className="text-white text-xl" onClick={toggleSidebar}>
