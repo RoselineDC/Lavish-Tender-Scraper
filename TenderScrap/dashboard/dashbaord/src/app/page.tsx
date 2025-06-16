709ca4c12594f493e46d46e9c8735ef494f70d63
RoselineDC <roseline.danga@gmail.com>
2025-06-16 13:01:37 +0200

    modified by Roseline on 2025-06-16 13:01:36

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -1,36 +1,35 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react'
-import UpNavBar from '@/components/Navbar/Navbar'
+import React, { useState } from "react";
+import UpNavBar from "@/components/Navbar/Navbar";
 import {
   Sidebar,
   SidebarHeader,
   SidebarBody,
   SidebarItem,
-} from '@/components/Navbar/sidebar'
+} from "@/components/Navbar/sidebar";
 export default function Page() {
-  const [showSidebar, setShowSidebar] = useState(false)
+  const [showSidebar, setShowSidebar] = useState(false);
 
   return (
     <>
-
-      <div className="flex-1 flex flex-col h-screen">
+      <div className="flex-1 flex flex-col h-scr">
         <UpNavBar toggleSidebar={() => setShowSidebar(!showSidebar)} />
         <main className="flex-1 overflow-y-auto">
           <h1 className="text-xl">Welcome to e-Tenders</h1>
         </main>
       </div>
-    <div className="flex  overflow-hidden">
-      {showSidebar && (
-        <Sidebar>
-          <SidebarHeader>Menu</SidebarHeader>
-          <SidebarBody>
-            <SidebarItem label="Dashboard" />
-            <SidebarItem label="Settings" />
-          </SidebarBody>
-        </Sidebar>
-      )}
-    </div>
+      <div className="flex  overflow-hidden">
+        {showSidebar && (
+          <Sidebar>
+            <SidebarHeader>Menu</SidebarHeader>
+            <SidebarBody>
+              <SidebarItem label="Dashboard" />
+              <SidebarItem label="Settings" />
+            </SidebarBody>
+          </Sidebar>
+        )}
+      </div>
     </>
-  )
+  );
 }
