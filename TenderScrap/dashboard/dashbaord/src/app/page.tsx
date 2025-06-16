d7e6aa8a6624e4be955ad7e97ee77f2ba37ce9cb
RoselineDC <roseline.danga@gmail.com>
2025-06-16 13:08:04 +0200

    modified by Roseline on 2025-06-16 13:08:04

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
           <Sidebar>
             <SidebarHeader>Menu</SidebarHeader>
             <SidebarBody>
-              <SidebarItem label="Profile"
+              <SidebarItem label="Profile">
                 <img
                   src="/profile.jpg"
                   alt="Profile"
