717b057bb97365b1966d6b9f5233189ca85cb958
RoselineDC <roseline.danga@gmail.com>
2025-06-16 22:05:59 +0200

    modified by Roseline on 2025-06-16 22:05:59

--- TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-interface ewIntents {
+interface NewIntents {
   id: number;
   institutionName: string;
   tender_number: string;
