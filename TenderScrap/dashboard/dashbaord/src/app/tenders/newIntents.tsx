9b2e84002dccafa599daf56918405722dcd091d8
RoselineDC <roseline.danga@gmail.com>
2025-06-16 22:39:48 +0200

    modified by Roseline on 2025-06-16 22:39:48

--- TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
@@ -39,7 +39,7 @@ const mockTenders: Tender[] = [
   },
 ];
 const filters = {
-  brand: ['Samsung', 'Apple', 'Huawei'],
+  I: ['Samsung', 'Apple', 'Huawei'],
   price: ['$0-100', '$101-500', '$501+'],
   device: ['PC', 'Tablet', 'Phone'],
   color: ['Purple', 'Black', 'White'],
