882e184db695876936482f60d009a2c0b3dd0769
RoselineDC <roseline.danga@gmail.com>
2025-06-16 15:43:21 +0200

    modified by Roseline on 2025-06-16 15:43:21

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -153,7 +153,15 @@ export default function Page() {
           </Sidebar>
         )}
       </div>
-      
+       <main className="flex-1 overflow-y-auto p-6 bg-white">
+          <h1 className="text-3xl font-bold text-gray-800 mb-4">
+            Welcome to e-Tenders
+          </h1>
+          <p className="text-gray-600">
+            This is your dashboard. Use the menu on the left to navigate through
+            Charts, Tenders, Forms, and more.
+          </p>
+        </main>
       </div>
     </>
   );
