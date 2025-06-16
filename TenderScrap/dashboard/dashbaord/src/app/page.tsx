dacf3e4ef60236c7e3a8eb09a93443647df7b19f
RoselineDC <roseline.danga@gmail.com>
2025-06-16 13:33:26 +0200

    modified by Roseline on 2025-06-16 13:33:26

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -62,7 +62,7 @@ export default function Page() {
               <SidebarItem label="Charts" />
               <SidebarItem label="Tables" />
               <SidebarItem label="Forms" />
-              <SidebarItem label="Ca" />
+              <SidebarItem label="Calender" />
               <SidebarItem label="Settings" />    
     
             </SidebarBody>
