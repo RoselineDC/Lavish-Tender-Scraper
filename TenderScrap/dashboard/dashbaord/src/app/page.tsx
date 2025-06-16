447decbeffb809d3decee56ac037593e5cd0a51b
RoselineDC <roseline.danga@gmail.com>
2025-06-16 14:02:00 +0200

    modified by Roseline on 2025-06-16 14:01:59

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -70,7 +70,7 @@ export default function Page() {
                      Monthly progress
                     </div>
                     <div className="hover:text-white cursor-pointer" onClick={() => alert("Clicked on Pie Chart")}>
-                      Pie Chart
+                      Pie Char
                     </div>
                   </div>
                 </div>
