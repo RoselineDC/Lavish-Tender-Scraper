ea87c80930c6223aa1dedb31c12f80c39a2961f0
RoselineDC <roseline.danga@gmail.com>
2025-06-16 16:50:37 +0200

    modified by Roseline on 2025-06-16 16:50:37

--- TenderScrap/dashboard/dashbaord/src/Routers/routers.tsx
+++ TenderScrap/dashboard/dashbaord/src/Routers/routers.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from "../components/Dashboard/dashboard";
-import Tenders from '.c/pages/Tenders';
+import Tenders from '.comp/pages/Tenders';
 import Tasks from './pages/Tasks';
 import Analytics from './pages/Analytics';
 import Downloads from './pages/Downloads';
