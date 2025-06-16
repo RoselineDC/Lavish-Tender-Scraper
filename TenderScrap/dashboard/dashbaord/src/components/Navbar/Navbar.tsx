b22688ef72b7fd5a72ce7f90295370dcfb1e3990
RoselineDC <roseline.danga@gmail.com>
2025-06-16 11:34:45 +0200

    modified by Roseline on 2025-06-16 11:34:43

--- TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
+++ TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
@@ -45,7 +45,7 @@ export default function UpNavBar({ toggleSidebar }: { toggleSidebar: () => void 
         {/* User Avatar */}
         <img
           src="" 
-          alt="User"
+          alt="P"
           className="w-8 h-8 rounded-full border border-white"
         />
 
