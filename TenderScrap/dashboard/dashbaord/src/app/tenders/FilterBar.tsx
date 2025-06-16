dba164e6844f9bb988a97b7294a6e707501af2c7
RoselineDC <roseline.danga@gmail.com>
2025-06-16 23:03:15 +0200

    modified by Roseline on 2025-06-16 23:03:15

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -71,7 +71,7 @@ export default function FilterBar() {
         </form>
         </div>
     </div>
-    =
+    
 
       {/* Actions Button */}
       <button className="flex items-center gap-2 px-4 py-2 border rounded-md hover:bg-gray-100 text-sm font-medium">
