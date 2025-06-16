119fd85de80dcde0e7ad8a4f7e1ecaf44d30a7fe
RoselineDC <roseline.danga@gmail.com>
2025-06-16 23:01:00 +0200

    modified by Roseline on 2025-06-16 23:01:00

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -38,7 +38,7 @@ export default function FilterBar() {
   };
 
   return (
- <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+ <div classN="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
             <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                 <div class="w-full md:w-1/2">
                     <form class="flex items-center">
