eb3d222f25853f24dd3866fd566ad72d26aaf4b1
RoselineDC <roseline.danga@gmail.com>
2025-06-16 15:59:02 +0200

    modified by Roseline on 2025-06-16 15:59:02

--- TenderScrap/dashboard/dashbaord/src/components/Dashboard/dashboard.tsx
+++ TenderScrap/dashboard/dashbaord/src/components/Dashboard/dashboard.tsx
@@ -4,7 +4,7 @@ import { LineChart, Line, PieChart, Pie, Cell } from "recharts";
 
 const pieData = [
   { name: "Submited", value: 16.85, color: "#ff4d4d" },
-  { name: "s", value: 45.36, color: "#4CAF50" },
+  { name: "", value: 45.36, color: "#4CAF50" },
   { name: "New Intents", value: 50.69, color: "#2196F3" },
 //    { name: "Cancelled", value: 50.69, color: "#FFC107" },
 //   { name: "Schedulled", value: 50.69, color: "#9C27B0" },
