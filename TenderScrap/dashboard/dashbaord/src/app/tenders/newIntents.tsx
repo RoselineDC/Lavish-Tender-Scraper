18500dda0979e2cac5a088f00039f081bdaaff44
RoselineDC <roseline.danga@gmail.com>
2025-06-16 22:52:50 +0200

    modified by Roseline on 2025-06-16 22:52:50

--- TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import FilterBar from ''./FilterBar";
+import FilterBar from './FilterBar'./FilterBar";
 
 interface Tender {
   id: number;
