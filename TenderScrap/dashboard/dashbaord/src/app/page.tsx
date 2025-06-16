d8fed2b690caf2e9c9f204c5239978aa61488957
RoselineDC <roseline.danga@gmail.com>
2025-06-16 13:43:36 +0200

    modified by Roseline on 2025-06-16 13:43:36

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -69,7 +69,7 @@ export default function Page() {
                   </div>
                   <div className="ml-6 space-y-2 text-fuchsia-400">
                     <div className="hover:text-white cursor-pointer">
-                     T
+                     transnet
                     </div>
                     <div className="hover:text-white cursor-pointer">
                       ● Data tables
