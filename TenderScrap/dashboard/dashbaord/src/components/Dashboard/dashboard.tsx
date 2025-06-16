092997df68d7fc705ef8f724f953da6e23d84981
RoselineDC <roseline.danga@gmail.com>
2025-06-16 15:50:12 +0200

    modified by Roseline on 2025-06-16 15:50:12

--- TenderScrap/dashboard/dashbaord/src/components/Dashboard/dashboard.tsx
+++ TenderScrap/dashboard/dashbaord/src/components/Dashboard/dashboard.tsx
@@ -5,7 +5,7 @@ import {
 } from 'recharts';
 
 const pieData = [
-  { name: 'Youtub', value: 16.85, color: '#ff4d4d' },
+  { name: 'Submited', value: 16.85, color: '#ff4d4d' },
   { name: 'Facebook', value: 45.36, color: '#4CAF50' },
   { name: 'Twitter', value: 50.69, color: '#2196F3' },
 ];
