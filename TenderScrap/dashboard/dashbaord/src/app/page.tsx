6f962263c774e5297e0c5de2c3833e450908b470
RoselineDC <roseline.danga@gmail.com>
2025-06-16 13:42:30 +0200

    modified by Roseline on 2025-06-16 13:42:30

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -67,7 +67,7 @@ export default function Page() {
                     <Table className="w-4 h-4 mr-2" />
                     <span className="font-small text-white">Tables</span>
                   </div>
-                  <div className="ml-6 space-y-2 ">
+                  <div className="ml-6 space-y-2 text">
                     <div className="hover:text-white cursor-pointer">
                       ● Simple tables
                     </div>
