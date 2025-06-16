ef01fe85f68350c5b8b2a68e9597c7508cc8eef6
RoselineDC <roseline.danga@gmail.com>
2025-06-16 22:50:38 +0200

    modified by Roseline on 2025-06-16 22:50:37

--- TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
@@ -58,6 +58,7 @@ const TenderTable: React.FC = () => {
   };
 
   return (
+    
     <div className="relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-xl bg-clip-border">
       <table className="w-full text-left table-auto min-w-max">
         <thead className="bg-gray-100">
