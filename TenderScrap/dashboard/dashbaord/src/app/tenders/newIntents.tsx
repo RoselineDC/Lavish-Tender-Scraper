166291e7d01e67d54b04cd8546bbd886545cb9c9
RoselineDC <roseline.danga@gmail.com>
2025-06-16 22:47:03 +0200

    modified by Roseline on 2025-06-16 22:47:02

--- TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
@@ -63,7 +63,7 @@ const filters = {
 
 const TenderTable: React.FC = () => {
   const [tenders, setTenders] = useState<Tender[]>(mockTenders);
-  CO
+  const fil
 
   const handleApprove = (id: number) => {
     alert(`Tender ID ${id} approved.`);
