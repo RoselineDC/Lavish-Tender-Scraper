c01ccfece6eaaea54f5c3821aed41764a4ad4df1
RoselineDC <roseline.danga@gmail.com>
2025-06-16 23:51:12 +0200

    modified by Roseline on 2025-06-16 23:51:10

--- TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
@@ -67,7 +67,7 @@ const Dashboard: React.FC = () => {
         footer="11k download in App store"
       />
     </div>   
-    <h1 className="text-3xl font-bold text-center mb-6">Scrapped Tenders @ 10am</h1>
+    <
         <NewIntents />  
       {/* Charts & Graphs */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
