3c9a30355e12e2df0b5defd7b57a986657b50b7d
RoselineDC <roseline.danga@gmail.com>
2025-06-16 14:12:12 +0200

    modified by Roseline on 2025-06-16 14:12:12

--- TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
+++ TenderScrap/dashboard/dashbaord/src/components/Navbar/Navbar.tsx
@@ -56,7 +56,7 @@ export default function UpNavBar({ toggleSidebar }: { toggleSidebar: () => void 
         <FaCog className="text-white" />
       </div>
     </header>
-    <ashboard />
+    <Dashboard />
     </>
   );
 }
