4b6dc6591fb468561cffac1ac3fe1068bbe999ad
RoselineDC <roseline.danga@gmail.com>
2025-06-16 13:46:03 +0200

    modified by Roseline on 2025-06-16 13:46:03

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -62,7 +62,7 @@ export default function Page() {
               <SidebarItem label="Dashboard" />
               <SidebarItem label="Charts" />
               <SidebarItem label="Tables">
-                <div className="bg-gray-800 p-2 rounded-md w-240">
+                <div className="bg-gray-800 p-2 rounded-md w-full">
                   <div className="ml-6 space-y-2 text-fuchsia-400">
                     <div className="hover:text-white cursor-pointer">
                      transnet
