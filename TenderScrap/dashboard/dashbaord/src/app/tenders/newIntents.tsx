a26d57ad5434880206dfe0788e806969dff89dc4
RoselineDC <roseline.danga@gmail.com>
2025-06-16 23:40:45 +0200

    modified by Roseline on 2025-06-16 23:40:45

--- TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
@@ -113,7 +113,7 @@ const TenderTable: React.FC = () => {
                       d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
                     />
                   </svg>
-                  Add In
+                  Add Intent
                 </button>
                 <div className="flex items-center space-x-3 w-full md:w-auto">
                   <button
