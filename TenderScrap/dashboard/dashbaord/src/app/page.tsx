987da8ef8860daa2687756ac10377c289d86c53c
RoselineDC <roseline.danga@gmail.com>
2025-06-16 14:25:16 +0200

    modified by Roseline on 2025-06-16 14:25:15

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -59,7 +59,8 @@ export default function Page() {
             </SidebarHeader>
 
             <SidebarBody>
-              <SidebarItem label="Dashboard" />
+              <SidebarItem label="Dashboard">
+                <div className="bg-gray-800 p-2 rounded-md w-full">
               <SidebarItem label="Charts">  
                 <div className="bg-gray-800 p-2 rounded-md w-full">
                   <div className="ml-6 space-y-2 text-fuchsia-400">
