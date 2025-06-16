98a81d7b28f2457c5eb463695d40e78caab90749
RoselineDC <roseline.danga@gmail.com>
2025-06-16 13:22:34 +0200

    modified by Roseline on 2025-06-16 13:22:33

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -66,7 +66,7 @@ export default function Page() {
                   {/* Profile Section */}
                   <div className="flex items-center space-x-4 mb-4">
                     <img
-                      src="/profile.jpg" // Update path as needed
+                      src="/profil.jpg" // Update path as needed
                       alt="Profile"
                       className="w-12 h-12 rounded-full border border-white"
                     />
