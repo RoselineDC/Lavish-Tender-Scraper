eba6c69a184020a9c1af3f55b51a7d51574f78cb
RoselineDC <roseline.danga@gmail.com>
2025-06-16 13:49:34 +0200

    modified by Roseline on 2025-06-16 13:49:34

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -61,7 +61,7 @@ export default function Page() {
             <SidebarBody>
               <SidebarItem label="Dashboard" />
               <SidebarItem label="Charts" />
-              <SidebarItem label="Ta">
+              <SidebarItem label="TENDERS">
                 <div className="bg-gray-800 p-2 rounded-md w-full">
                   <div className="ml-6 space-y-2 text-fuchsia-400">
                     <div className="hover:text-white cursor-pointer" onClick={() => alert("Clicked on Transnet")}>
