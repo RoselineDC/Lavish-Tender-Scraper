7d5475a64136d9c60d5f0737c91301ff358322f5
RoselineDC <roseline.danga@gmail.com>
2025-06-16 14:44:08 +0200

    modified by Roseline on 2025-06-16 14:44:07

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import UpNavBar from "@/components/Navbar/Navbar";
-import { Search, LayoutDashboard, Table, Cha } from "lucide-react";
+import { Search, LayoutDashboard, Table, Chat } from "lucide-react";
 import { ChartSpline } from 'lucide-react';
 
 // import { Table } from "lucide-react";
