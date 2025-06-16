6103e5e6c1615a5c66c108372c80b76db67cfa9f
RoselineDC <roseline.danga@gmail.com>
2025-06-16 14:51:07 +0200

    modified by Roseline on 2025-06-16 14:51:06

--- TenderScrap/dashboard/dashbaord/src/app/page.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import UpNavBar from "@/components/Navbar/Navbar";
-import { Search, LayoutDashboard, Table, ChartSpline } from "lucide-react";
+import { Search, LayoutDashboard, Table, ChartSpline, BookType } from "lucide-react";
 
 // import { Table } from "lucide-react";
 // import Dashboard from "../components/Dashboard/dashboard";
