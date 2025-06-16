b39fb58642fb0d1526ff404a04bac7524161ebd9
RoselineDC <roseline.danga@gmail.com>
2025-06-16 14:14:08 +0200

    modified by Roseline on 2025-06-16 14:14:07

--- TenderScrap/dashboard/dashbaord/src/components/Dashboard/dashboard.tsx
+++ TenderScrap/dashboard/dashbaord/src/components/Dashboard/dashboard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const dashboard = () => {
   return (
-    <div >
+    <div className="flex items-center justify-center h-screen bg-gray-100 text-gray-800">
         dashboard
         </div>
   )
