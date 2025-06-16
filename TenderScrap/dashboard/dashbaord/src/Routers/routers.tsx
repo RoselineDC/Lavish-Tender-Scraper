1fa7661d67f211c76a57a28871d224b6c57549fe
RoselineDC <roseline.danga@gmail.com>
2025-06-16 16:54:48 +0200

    modified by Roseline on 2025-06-16 16:54:47

--- TenderScrap/dashboard/dashbaord/src/Routers/routers.tsx
+++ TenderScrap/dashboard/dashbaord/src/Routers/routers.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from "../components/Dashboard/dashboard";
 import Tenders from '../components/Pages/Tenders';
 import Tasks from '../components/Pages/Suppliers';
-import Analytics from './pages/Analytics';
+import Analytics from '../components/Pages/Analytics';
 import Downloads from './pages/Downloads';
 
 function App() {
--- TenderScrap/dashboard/dashbaord/src/components/Pages/Analytics.tsx
+++ TenderScrap/dashboard/dashbaord/src/components/Pages/Analytics.tsx
@@ -1,1 +1,9 @@
-raf
+import React from 'react'
+
+const Analytics = () => {
+  return (
+    <div>Analytics</div>
+  )
+}
+
+export default Analytics
