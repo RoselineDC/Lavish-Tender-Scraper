566edde5a5dac0e53ed6d98b33d469fb3d9e3192
RoselineDC <roseline.danga@gmail.com>
2025-06-16 13:56:27 +0200

    modified by Roseline on 2025-06-16 13:56:27

--- TenderScrap/dashboard/dashbaord/src/app/layout.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/layout.tsx
@@ -10,9 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"], weight: ['300', '400', '500', '600', '700'] });
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
-import dashboard from "<div styleName={} />
-<components />
-<Dashboar></Dashboar>/dashboard";
+import dashboard from "../components/Dashboar/dashboard";
 
 export const metadata: Metadata = {
   title: "Lavish DEALS",
