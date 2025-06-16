584bce9659d3e5ef97af1cf91d9ecf5d73bc8660
RoselineDC <roseline.danga@gmail.com>
2025-06-16 22:58:59 +0200

    modified by Roseline on 2025-06-16 22:58:58

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { FaChevronDown } from 'react-icons/fa';
+import { useState } from "react";
+import { FaChevronDown } from "react-icons/fa";
 
 const filters = {
   institutionName: ["Transnet", "CSIR", "OTHERS"],
