485f6e72f99dfa4e0bd8ef70efd242aedca04e8f
RoselineDC <roseline.danga@gmail.com>
2025-06-16 17:20:51 +0200

    modified by Roseline on 2025-06-16 17:20:51

--- TenderScrap/dashboard/dashbaord/src/components/Dashboard/dashboard.tsx
+++ TenderScrap/dashboard/dashbaord/src/components/Dashboard/dashboard.tsx
@@ -30,7 +30,7 @@ const Dashboard: React.FC = () => {
   return (
    <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
   <Card
-    link="../../app/Pages/Tenders"
+    link="/Pages/Tenders"
     color="orange"
     title="Tenders"
     subtitle="All Tenders"
