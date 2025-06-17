539c9508cd957195865ac74dd0f7d32fd66f38a9
RoselineDC <roseline.danga@gmail.com>
2025-06-17 21:49:01 +0200

    modified by Roseline on 2025-06-17 21:49:01

--- TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
@@ -73,7 +73,7 @@ const Dashboard: React.FC = () => {
 
         {/* Pie Chart */}
         <div className="bg-white rounded-xl p-4 shadow-md">
-          <h3 className="text-lg font-bold mb-2 p-4">Total Revenue</h3>
+          <h3 className="text-lg font-bold p-4">Total Revenue</h3>
           <PieChart width={300} height={200}>
             <Pie data={pieData} dataKey="value" outerRadius={80} label>
               {pieData.map((entry, index) => (
