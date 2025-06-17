3b2be59905378f849e09cef89093cdc0084021ae
RoselineDC <roseline.danga@gmail.com>
2025-06-17 22:03:37 +0200

    modified by Roseline on 2025-06-17 22:03:37

--- TenderScrap/dashboard/dashbaord/src/app/analytics/piechart.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/analytics/piechart.tsx
@@ -24,7 +24,7 @@ ChartJS.register(
 );
 
 const MonthlyRecapChart = () => {
-  const labels = ["January", "February", "March", "April", "May", "June", "July, "];
+  const labels = ["January", "February", "March", "April", "May", "June", "July, August, September, October, November, December"];
 
   const data = {
     labels,
