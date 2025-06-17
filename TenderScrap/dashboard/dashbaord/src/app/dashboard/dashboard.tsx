6fd34874ce3b451714a98c429402dac57f26c100
RoselineDC <roseline.danga@gmail.com>
2025-06-17 21:41:44 +0200

    modified by Roseline on 2025-06-17 21:41:42

--- TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { LineChart, Line, PieChart, Pie, Cell } from "recharts";
+
 import Link from "next/link";
 import NewIntents from "@/app/tenders/NewIntents";
 import MonthlyRecapChart from "@/app/analytics/piechart";
