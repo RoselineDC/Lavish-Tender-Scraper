64ac1eb6a768b91e096cd15c414bdad2aa820781
RoselineDC <roseline.danga@gmail.com>
2025-06-17 20:52:36 +0200

    modified by Roseline on 2025-06-17 20:52:36

--- TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
@@ -17,6 +17,7 @@ const pieData = [
   { name: "Not Submitted", value: 16.85, color: "#ff4d4d" },
   { name: "Submitted", value: 45.36, color: "#4CAF50" },
   { name: "New Intents", value: 50.69, color: "#2196F3" },
+  
 ];
 
 const lineData = [
