4042a3a8afebd17519a27ee5046eb2d741b606b6
RoselineDC <roseline.danga@gmail.com>
2025-06-17 21:36:58 +0200

    modified by Roseline on 2025-06-17 21:36:58

--- TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
@@ -11,7 +11,7 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
-
+import { Line } from "react-chartjs-2";
 import Link from "next/link";
 import NewIntents from "@/app/tenders/NewIntents";
 // Update the path below to the correct relative path based on your project structure.
