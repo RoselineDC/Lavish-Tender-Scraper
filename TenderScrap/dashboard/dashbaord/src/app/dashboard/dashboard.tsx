0060824ef713e91797c1286315f942b9099404f1
RoselineDC <roseline.danga@gmail.com>
2025-06-17 21:45:03 +0200

    modified by Roseline on 2025-06-17 21:45:02

--- TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
@@ -16,8 +16,8 @@ import MonthlyRecapChart from "@/app/analytics/piechart";
 
 const pieData = [
   { name: "Not Submitted", value: 16.85, color: "#ff4d4d" },        
-  { name: "Submitted", value: 45.36, color: "#4CAF50" },            // Green
-  { name: "New Intents", value: 50.69, color: "#2196F3" },          // Blue
+  { name: "Submitted", value: 45.36, color: "#4CAF50" },           
+  { name: "New Intents", value: 50.69, color: "#2196F3" },         
   { name: "Submission For Tomorrow", value: 20.69, color: "#FFC107" } // Amber/Yellow
 ];
 
