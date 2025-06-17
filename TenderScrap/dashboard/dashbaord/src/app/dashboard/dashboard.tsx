1edcbee71594bf2a6ae417e77091b0f41dbc49eb
RoselineDC <roseline.danga@gmail.com>
2025-06-17 23:02:40 +0200

    modified by Roseline on 2025-06-17 23:02:39

--- TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
@@ -66,7 +66,9 @@ const Dashboard: React.FC = () => {
       {/* Charts & Graphs */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
         {/* Sales Per Day */}
-        div
+        <div>
+          
+        </div>
 
         <MonthlyRecapChart />
 
