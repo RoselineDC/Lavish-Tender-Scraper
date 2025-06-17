2ef4431b8065a585d0f6d219eff1ab732e19a652
RoselineDC <roseline.danga@gmail.com>
2025-06-17 22:55:48 +0200

    modified by Roseline on 2025-06-17 22:55:47

--- TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
@@ -71,7 +71,7 @@ const Dashboard: React.FC = () => {
 
         {/* Pie Chart */}
         <div className="bg-white rounded-xl p-4 shadow-md flex flex-col items-center">
-          
+          <
             <h3 className="text-lg font-bold mb-2 flex flex-col items-center">
               Total Revenue
             </h3>
