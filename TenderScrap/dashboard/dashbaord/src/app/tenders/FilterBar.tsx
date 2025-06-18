86e2b3a3c54d32c58cdad393baa9572823ba97c6
RoselineDC <roseline.danga@gmail.com>
2025-06-18 20:58:46 +0200

    modified by Roseline on 2025-06-18 20:58:43

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -41,7 +41,7 @@ export default function FilterBar() {
     setSelected({ ...selected, [key]: value });
   };
   // Hhandle search 
-  const handleSearch
+  const handleSearch()
 
   return (
     <>
