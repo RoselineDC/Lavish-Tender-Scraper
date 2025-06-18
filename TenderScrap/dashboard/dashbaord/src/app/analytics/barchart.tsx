8e71c936196cfac64cdeb29bc3851bef436820db
RoselineDC <roseline.danga@gmail.com>
2025-06-18 09:10:58 +0200

    modified by Roseline on 2025-06-18 09:10:58

--- TenderScrap/dashboard/dashbaord/src/app/analytics/barchart.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/analytics/barchart.tsx
@@ -56,7 +56,7 @@ const barChartsParams = {
       },
     },
   ],
-  xAxis: [{ data: ["week 1", "week 2", "week 3", "week 4", "week 5", "week 6"], id: "axis1" }],
+  xAxis: [{ data: ["week 1", "week 2", "week 3", "week 4", "week 5", "week 6", "week 7", "week 8", "week 9", "week 10"], id: "axis1" }],
   height: 400,
 } as const;
 
