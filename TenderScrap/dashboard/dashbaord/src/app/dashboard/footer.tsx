9874b4c8e894d01c9da570d41132fabd9232bec7
RoselineDC <roseline.danga@gmail.com>
2025-06-18 16:20:04 +0200

    modified by Roseline on 2025-06-18 16:20:04

--- TenderScrap/dashboard/dashbaord/src/app/dashboard/footer.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/dashboard/footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
           <div className="mb-6 md:mb-0">
             <a href="https://flowbite.com/" className="flex items-center">
               <img
-                src="https://flowbite.com/docs/images/logo.sv"
+                src="https://"
                 className="h-8 me-3"
                 alt="FlowBite Logo"
               />
