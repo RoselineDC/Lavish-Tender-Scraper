0625b707efc7ca778040ea1097ec2b645ef84534
RoselineDC <roseline.danga@gmail.com>
2025-06-18 22:46:23 +0200

    modified by Roseline on 2025-06-18 22:46:22

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -403,7 +403,7 @@ export default function FilterBar() {
                     </td>
                   </tr>
                 ) : (
-                  {]filteredTenders.map((tender, index) => () {
+                  filteredTenders.map((tender, index) => () {
                      <tr 
                       key={index}
                       className="border-b dark:border-gray-700"
@@ -464,7 +464,7 @@ export default function FilterBar() {
                         
                           </td>
                           </tr>
-                  })}
+                  })
                           
                           </tbody>
 
