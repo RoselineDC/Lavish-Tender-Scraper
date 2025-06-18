6f3e0840239ddaf609d9d0e84d79dedfd4665324
RoselineDC <roseline.danga@gmail.com>
2025-06-18 22:37:15 +0200

    modified by Roseline on 2025-06-18 22:37:14

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -439,7 +439,25 @@ export default function FilterBar() {
                       </td>
                       <td className="px-4 py-3 flex items-center justify-end">
                         <button
-                          
+                          id={`tender-actions-dropdown-button-${index}`}
+                          data-dropdown-toggle={`tender-actions-dropdown-${index}`}
+                          className="inline-flex items-center p-0.5 text-sm font-medium text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
+                          type="button"
+                        >
+                          <svg
+                            className="w-5 h-5"
+                            aria-hidden="true"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                          </svg>
+                        </button>
+                        <div
+                          id={`tender-actions-dropdown-${index}`}
+                          className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
+                        >
                   ))}
                   {/* <tr className="border-b dark:border-gray-700"> */}
                     <th
