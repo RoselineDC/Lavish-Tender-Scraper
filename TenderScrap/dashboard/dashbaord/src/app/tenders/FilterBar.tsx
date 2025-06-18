d4feef6f698e31323f41145cffe7b7994a4ba186
RoselineDC <roseline.danga@gmail.com>
2025-06-18 23:08:01 +0200

    modified by Roseline on 2025-06-18 23:07:59

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -169,7 +169,7 @@ export default function FilterBar() {
                     title="Refresh the table"
                   >
                     <span className="sr-only">
-                      
+                      <FcRefresh />
                       Refresh</span>
                     
                   </button>
