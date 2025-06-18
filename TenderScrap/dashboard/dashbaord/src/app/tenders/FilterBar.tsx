ee8df77f126f2f6a6657931185bec3bd5824394f
RoselineDC <roseline.danga@gmail.com>
2025-06-18 21:47:21 +0200

    modified by Roseline on 2025-06-18 21:47:20

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -334,7 +334,7 @@ export default function FilterBar() {
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
                     <th scope="col" className="px-4 py-3">
-                      {tenders.institution ame}
+                      {tenders.institutionname}
                     </th>
                     <th scope="col" className="px-4 py-3">
                       Tender Number
