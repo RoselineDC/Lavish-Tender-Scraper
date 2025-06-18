45ee2ce8dfc67a1f6a86c3ab9e3bb412341b9255
RoselineDC <roseline.danga@gmail.com>
2025-06-18 08:59:38 +0200

    modified by Roseline on 2025-06-18 08:59:37

--- TenderScrap/dashboard/dashbaord/src/app/analytics/barchart.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/analytics/barchart.tsx
@@ -92,7 +92,7 @@ export default function BarClick() {
           </IconButton>
         </Box>
         <HighlightedCode
-          code={`Overview of the Month
+          code={`Overview of the Monthly Progre
 ${itemData ? JSON.stringify(itemData, null, 2) : '// The data will appear here'}
 
 // Data from axis click
