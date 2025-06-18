6c5230bc182a831047c450c9b4156f0056277fb4
RoselineDC <roseline.danga@gmail.com>
2025-06-18 22:53:46 +0200

    modified by Roseline on 2025-06-18 22:53:46

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -438,12 +438,12 @@ export default function FilterBar() {
                           {tender.tender_status}
                         </span>
                       </td>
-                      <td className="px-4 py-3">
+                      {/* <td className="px-4 py-3">
                         <div className="text-sm">
                           <p className="font-medium">{tender.contact_person}</p>
                           <p className="text-xs text-blue-600">{tender.contact_email}</p>
                         </div>
-                      </td>
+                      </td> */}
                       <td className="px-4 py-3 flex items-center justify-end">
                         <button
                           id={`tender-actions-dropdown-button-${index}`}
