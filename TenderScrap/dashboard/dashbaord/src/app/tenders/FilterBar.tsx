64c5c64fe0e3404efa927066a21a558eee5db9f2
RoselineDC <roseline.danga@gmail.com>
2025-06-18 23:19:26 +0200

    modified by Roseline on 2025-06-18 23:19:26

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -170,7 +170,7 @@ export default function FilterBar() {
                     title="Refresh the table"
                   >
                     <FcRefresh className="w-6 h-6 font-bold" />
-                    Rrfresh
+                    Refresh
                     
                   </button>
 
@@ -184,19 +184,7 @@ export default function FilterBar() {
                     <TfiFilter />
 
                     Filter
-                    <svg
-                      className="-mr-1 ml-1.5 w-5 h-5"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                    >
-                      <path
-                        clipRule="evenodd"
-                        fillRule="evenodd"
-                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      />
-                    </svg>
+                    
                   </button>
                   <div
                     id="filterDropdown"
