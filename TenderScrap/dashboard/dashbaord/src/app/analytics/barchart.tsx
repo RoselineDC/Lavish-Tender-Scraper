1b4028f2f43d243907a0db8ff43d9e47d59ec284
RoselineDC <roseline.danga@gmail.com>
2025-06-18 08:47:07 +0200

    modified by Roseline on 2025-06-18 08:47:07

--- TenderScrap/dashboard/dashbaord/src/app/analytics/barchart.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/analytics/barchart.tsx
@@ -66,35 +66,6 @@ export default function BarClick() {
 
   return (
     <Stack
-      <Stack direction="column" sx={{ width: { xs: "100%", md: "40%" } }}>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <Typography>Click on the chart</Typography>
-          <IconButton
-            aria-label="reset"
-            size="small"
-            onClick={() => {
-              setItemData(undefined);
-              setAxisData(null);
-            }}
-          >
-            <UndoOutlinedIcon fontSize="small" />
-          </IconButton>
-        </Box>
-        <HighlightedCode
-          code={`Overview of the Monthly Progress
-${itemData ? JSON.stringify(itemData, null, 2) : "// The data will appear here"}
-
-// Data from axis click
-${axisData ? JSON.stringify(axisData, null, 2) : "// The data will appear here"}
-`}
-        />
-      </Stack>
       className="bg-white rounded-xl p-4 shadow-md"
       direction={{ xs: "column", md: "row" }}
       spacing={{ xs: 0, md: 4 }}
