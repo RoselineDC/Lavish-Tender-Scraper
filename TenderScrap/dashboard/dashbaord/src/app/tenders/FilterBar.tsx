8161b86f1364b2a0c583642e107f39bbba41f0f3
RoselineDC <roseline.danga@gmail.com>
2025-06-18 23:27:43 +0200

    modified by Roseline on 2025-06-18 23:27:42

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -205,7 +205,7 @@ export default function FilterBar() {
                         id="filterDropdown"
                         className="absolute right-0 z-10 mt-2 w-48 p-3 bg-white rounded-lg shadow dark:bg-gray-700"
                       >
-                        <h6 className="mb-3 text-sm font-medium text-gray-900 dark:text-white bg-gr">
+                        <h6 className="mb-3 text-sm font-medium text-gray-900 dark:text-white bg-gre">
                           Choose Category
                         </h6>
                         <ul className="space-y-2 text-sm">
