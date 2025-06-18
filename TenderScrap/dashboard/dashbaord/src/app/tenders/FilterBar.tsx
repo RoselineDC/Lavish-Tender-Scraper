c75ac25485da67bf20176f61d56f77703f6d83ae
RoselineDC <roseline.danga@gmail.com>
2025-06-18 23:16:35 +0200

    modified by Roseline on 2025-06-18 23:16:34

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -179,7 +179,8 @@ export default function FilterBar() {
                     className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                     type="button"
                   >
-                    
+                    <TfiFilter />
+
                     Filter
                     <svg
                       className="-mr-1 ml-1.5 w-5 h-5"
