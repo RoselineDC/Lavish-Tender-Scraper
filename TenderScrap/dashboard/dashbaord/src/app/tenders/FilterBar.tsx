4a2512e1b8029f4eeb8a6783d32dbec5df9bf587
RoselineDC <roseline.danga@gmail.com>
2025-06-18 22:47:43 +0200

    modified by Roseline on 2025-06-18 22:47:41

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -464,6 +464,7 @@ export default function FilterBar() {
                         
                           </td>
                           </tr>
+                          
                   )}
                           
                           </tbody>
