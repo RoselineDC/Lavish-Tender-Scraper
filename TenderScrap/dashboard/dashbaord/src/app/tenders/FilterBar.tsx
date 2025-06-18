dad08b4d74a4420602af1db11eedbd9d4cc740dd
RoselineDC <roseline.danga@gmail.com>
2025-06-18 22:32:48 +0200

    modified by Roseline on 2025-06-18 22:32:46

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -403,6 +403,8 @@ export default function FilterBar() {
                     >
                       <td className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         {tender.institutionName}
+                      </td>
+                      <td className="px-4 py-3">{tender.tenderNumber}</td>
                   ))}
                   {/* <tr className="border-b dark:border-gray-700"> */}
                     <th
