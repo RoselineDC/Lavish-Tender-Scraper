f3f28b31bd1f95822dcc7aed718ed82fcc13925c
RoselineDC <roseline.danga@gmail.com>
2025-06-18 22:59:46 +0200

    modified by Roseline on 2025-06-18 22:59:45

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -168,7 +168,7 @@ export default function FilterBar() {
                   >
                     <svg
                       className="-ml-1 mr-1.5 w-5 h-5"
-                      fill="b"
+                      fill="blue"
                       viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg"
                       aria-hidden="true"
