77e5401c2fe1434f9e7176cb489b6cde3743e67c
RoselineDC <roseline.danga@gmail.com>
2025-06-18 09:16:20 +0200

    modified by Roseline on 2025-06-18 09:16:19

--- TenderScrap/dashboard/dashbaord/src/app/analytics/barchart.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/analytics/barchart.tsx
@@ -56,7 +56,7 @@ const barChartsParams = {
       },
     },
   ],
-  xAxis: [{ data: ["Jan", "Feb", "Mar", "April", "May", "June", "July", "August", "September", "October", "November", "December"], id: "axis0" }, 
+  xAxis: [{ data: ["Jan", "Feb", "Mar", "April", "May", "June", "July", "August", "September", "October", "November", "December"], id: "axis0" }
 } as const;
 
 export default function BarClick() {
