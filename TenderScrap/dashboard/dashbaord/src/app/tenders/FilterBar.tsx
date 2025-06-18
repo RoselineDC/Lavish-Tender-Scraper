68f2931b8c559ab7677c9d0df11be2487fa9bc15
RoselineDC <roseline.danga@gmail.com>
2025-06-18 20:11:39 +0200

    modified by Roseline on 2025-06-18 20:11:38

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -52,6 +52,7 @@ export default function FilterBar() {
                 <form className="flex items-center">
                   <label htmlFor="simple-search" className="sr-only">
                     Search
+                    
                   </label>
                   <div className="relative w-full">
                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
