41c158e36368919985b0bb0198550f5e94e71be0
RoselineDC <roseline.danga@gmail.com>
2025-06-18 16:20:33 +0200

    modified by Roseline on 2025-06-18 16:20:33

--- TenderScrap/dashboard/dashbaord/src/app/dashboard/footer.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/dashboard/footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <a href="https://.com/" className="flex items-center">
+            <a href="https://LavishTech.com/" className="flex items-center">
               <img
                 src="https://flowbite.com/docs/images/logo.svg"
                 className="h-8 me-3"
