3d9aff68729878b24a4390314b4d7339cab4c626
RoselineDC <roseline.danga@gmail.com>
2025-06-28 13:19:46 +0200

    modified by Roseline on 2025-06-28 13:19:43

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -131,7 +131,7 @@ export default function FilterBar() {
   };
 
   return (
-    <div className="className="bg-white rounded-xl p-4 shadow-md  border-t-4 border-green-500 hover:shadow-lg transition">mx-auto max-w-screen-xl p-3 sm:p-5 px-4 lg:px-1">
+    <div className="g-white rounded-xl p-4 shadow-md  border-t-4 border-green-500 hover:shadow-lg transition">mx-auto max-w-screen-xl p-3 sm:p-5 px-4 lg:px-1">
       {/* Start coding here */}
       <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
         <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
