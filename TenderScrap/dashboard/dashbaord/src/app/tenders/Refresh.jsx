76e645753c238629933e130d27785402a1ee1411
RoselineDC <roseline.danga@gmail.com>
2025-06-28 10:47:23 +0200

    modified by Roseline on 2025-06-28 10:47:22

--- TenderScrap/dashboard/dashbaord/src/app/tenders/Refresh.jsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/Refresh.jsx
@@ -3,3 +3,12 @@
 
 import React from 'react'
 
+export default function Refresh() {
+    return (
+        <div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Refresh
+            </button>
+        </div>
+    )
+}
