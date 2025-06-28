0a3a6e3b8b76f137bd0157286aedc17a7fb07367
RoselineDC <roseline.danga@gmail.com>
2025-06-28 13:17:55 +0200

    modified by Roseline on 2025-06-28 13:17:52

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -133,7 +133,7 @@ export default function FilterBar() {
   return (
    
       <section className="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
-        <div className="mx-auto max-w-screen-xl  px-4 lg:px-1">
+        <div className="mx-auto max-w-screen-xl p-3 sm:p-5 px-4 lg:px-1">
           {/* Start coding here */}
           <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
             <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
