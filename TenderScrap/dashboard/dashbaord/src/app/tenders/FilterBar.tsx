3ac1709109cd04c91f3766722160d1f27d1e97dc
RoselineDC <roseline.danga@gmail.com>
2025-06-28 14:07:42 +0200

    modified by Roseline on 2025-06-28 14:07:41

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -124,7 +124,7 @@ export default function FilterBar() {
   const toggleDropdown = () => setShowDropdown(!showDropdown);
 
   // filter tenders based on search term
- filteredTenders = tenders.filter((tender) =>
+  const filteredTenders = tenders.filter((tender) =>
     `${tender.institutionName} ${tender.tender_number} ${tender.description} ${tender.tender_category} ${tender.location} ${tender.contact_person} ${tender.contact_email}`
       .toLowerCase()
       .includes(searchTerm.toLowerCase())
