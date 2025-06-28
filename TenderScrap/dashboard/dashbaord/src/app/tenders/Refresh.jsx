03ba4ef94bb6ad622becfe328d98f65f071b24cb
RoselineDC <roseline.danga@gmail.com>
2025-06-28 11:03:49 +0200

    modified by Roseline on 2025-06-28 11:03:46

--- TenderScrap/dashboard/dashbaord/src/app/tenders/Refresh.jsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/Refresh.jsx
@@ -6,6 +6,7 @@ import React from 'react'
 export default function Refresh() {
     return (
         <div>
+              <FcRefresh className="w-6 h-6 font-bold" />
            
                 Refresh
         </div>
