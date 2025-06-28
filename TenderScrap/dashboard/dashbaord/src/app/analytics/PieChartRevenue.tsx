f7c5ac124750668a8fa8ab72016a585dcd383631
RoselineDC <roseline.danga@gmail.com>
2025-06-28 13:12:24 +0200

    modified by Roseline on 2025-06-28 13:12:23

--- TenderScrap/dashboard/dashbaord/src/app/analytics/PieChartRevenue.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/analytics/PieChartRevenue.tsx
@@ -43,7 +43,7 @@ import React from "react";
 import { useEffect, useRef } from 'react';
 
 // @ts-ignore – if no type definitions available
-import 'https://drilldowncharts.com/lib/drilldown.min.js'; // for external CDN usage
+?; // for external CDN usage
 const PieChartRevenue = () => {
   const chartRef = useRef<HTMLDivElement>(null);
 
