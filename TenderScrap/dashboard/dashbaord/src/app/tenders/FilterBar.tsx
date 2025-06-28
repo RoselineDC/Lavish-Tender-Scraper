ff57a54258d5cb16a53a6871d596d80ce84b7e74
RoselineDC <roseline.danga@gmail.com>
2025-06-28 14:02:28 +0200

    modified by Roseline on 2025-06-28 14:02:26

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -165,7 +165,7 @@ export default function FilterBar() {
             <div className="relative inline-block text-left w-full md:w-auto">
               <button
                 id="filterDropdownButton"
-                onClick={toggleDropdown}
+                onClick={toggleDropdon}
                 className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                 type="button"
               >
