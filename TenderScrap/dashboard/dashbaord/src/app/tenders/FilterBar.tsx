945f19b6b44c7f845c046689e99d28d37a814d2f
RoselineDC <roseline.danga@gmail.com>
2025-06-28 14:04:23 +0200

    modified by Roseline on 2025-06-28 14:04:22

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -35,7 +35,7 @@ export default function FilterBar() {
   const [searchTerm, setSearchTerm] = useState("");
   const [showDropdown, setShowDropdown] = useState(false);
   const [checkedFilters, setCheckedFilters] = useState([]);
-  const [filteredTenders, setFilteredTenders] = useState(tenders);
+  
 
   // store tender
   const [tenders, setTenders] = useState([
