2334a84b9c9a74264cd8ccb74515b470605a6313
RoselineDC <roseline.danga@gmail.com>
2025-06-28 11:32:58 +0200

    modified by Roseline on 2025-06-28 11:32:58

--- TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
@@ -5,7 +5,7 @@ import { PieChart, Pie, Cell } from "recharts";
 import Link from "next/link";
 
 import FilterBar from "@/app/tenders/FilterBar";
-import MonthlyRecapChart from "@/app/analytics/piechart";
+import PieChartRevenue from "../pieChartRevenu/analytics/piechart";
 import  BarClick from "../analytics/barchart";
 
 import Footer from "./footer";
