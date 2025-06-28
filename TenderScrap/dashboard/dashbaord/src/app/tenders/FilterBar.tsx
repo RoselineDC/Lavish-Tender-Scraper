c18c62b71dac117d34e65157ec9ef5f54bef4230
RoselineDC <roseline.danga@gmail.com>
2025-06-28 13:26:20 +0200

    modified by Roseline on 2025-06-28 13:26:16

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -171,7 +171,7 @@ export default function FilterBar() {
                   <button
                     id="refreshButton"
                     onClick={handleRefresh}
-                    className=" md:w-auto flex items-center justify-center py-2 px-4 text-sm font-bold text-white focus:outline-none bg-green-500 rounded-lg border hover:bg-green-900 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-green-200 dark:focus:ring-gray-700 dark:bg-gray-800 00"
+                    className=" md:w-auto flex items-center justify-center py-2 px-4 text-sm font-bold text-white focus:outline-none bg-green-500 rounded-lg border hover:bg-green-900 hover:text-primary-700focus:z-10 focus:ring-4 focus:ring-green-200 dark:focus:ring-gray-700 dark:bg-gray-800 00"
                     type="button"
                     title="Refresh the table"
                   >
