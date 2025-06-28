70018f722f916f838733f2370196feb27ce105a6
RoselineDC <roseline.danga@gmail.com>
2025-06-28 13:18:05 +0200

    modified by Roseline on 2025-06-28 13:18:01

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -131,8 +131,6 @@ export default function FilterBar() {
   }
 
   return (
-   
-      <section className="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
         <div className="mx-auto max-w-screen-xl p-3 sm:p-5 px-4 lg:px-1">
           {/* Start coding here */}
           <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
@@ -473,7 +471,6 @@ export default function FilterBar() {
           </div>
         </div>
       
-    </ section>
   );
 }
 // pagination
