87da5a6da1feb087439dfa43d7b489963de00864
RoselineDC <roseline.danga@gmail.com>
2025-07-05 19:18:03 +0200

    modified by Roseline on 2025-07-05 19:18:03

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -50,7 +50,7 @@ export default function FilterBar() {
       tender_document_url:         "https://transnetetenders.azurewebsites.net/Home/TenderDetails?Id=10013",
       tender_category: "Supplies",
       tender_type: "Open",
-      tender_status: "C",
+      tender_status: "Closed",
       contact_person: "John Doe",
       contact_email: "john@example.com",
     },
