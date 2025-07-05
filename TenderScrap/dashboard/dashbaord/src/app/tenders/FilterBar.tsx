cb43767ee9ccdbe60af42264a039d7005f34b793
RoselineDC <roseline.danga@gmail.com>
2025-07-05 19:36:12 +0200

    modified by Roseline on 2025-07-05 19:36:11

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -299,7 +299,7 @@ const handleDelete = (id: number) => {
           </div>
         </div>
       </div>
-      <div className="overflow-hidden">
+      <div className="overflow-h">
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
