c3dc0e93d710a8eeae29fe1cf34f8104bca00310
RoselineDC <roseline.danga@gmail.com>
2025-07-05 22:44:10 +0200

    modified by Roseline on 2025-07-05 22:44:10

--- TenderScrap/dashboard/dashbaord/src/app/dashboard/TableWithPagination.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/dashboard/TableWithPagination.tsx
@@ -15,7 +15,7 @@ const TableWithPagination = () => {
     currentPage * itemsPerPage
   );
 
-  const handlePageClick = (page: SetStateAction<) => {
+  const handlePageClick = (page: number) => {
     if (page >= 1 && page <= totalPages) {
       setCurrentPage(page);
     }
