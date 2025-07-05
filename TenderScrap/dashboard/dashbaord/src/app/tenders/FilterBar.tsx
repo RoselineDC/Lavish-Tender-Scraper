1dd2acc71405bbaf19ae674687ef0da926ff0fc7
RoselineDC <roseline.danga@gmail.com>
2025-07-05 22:34:59 +0200

    modified by Roseline on 2025-07-05 22:34:57

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -462,7 +462,7 @@ export default function FilterBar() {
           <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
             Showing
             <span className="font-semibold text-gray-900 dark:text-white">
-              1-10
+              1- 10
             </span>
             of
             <span className="font-semibold text-gray-900 dark:text-white">
