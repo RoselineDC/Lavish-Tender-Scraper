f32f22f99ae0adbdaf3c9e040eec0344552f75de
RoselineDC <roseline.danga@gmail.com>
2025-07-05 22:20:44 +0200

    modified by Roseline on 2025-07-05 22:20:40

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -193,6 +193,7 @@ const handleDelete = (id: number) => {
 };
 
 
+
   return (
     <div className="bg-white rounded-xl p-4 shadow-md  border-t-4 border-green-500 hover:shadow-lg transition ">
       <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
