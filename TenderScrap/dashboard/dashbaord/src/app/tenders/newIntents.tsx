98b9c9af75a1766d12c73b4e1f0cef0474c691fe
RoselineDC <roseline.danga@gmail.com>
2025-07-07 01:45:38 +0200

    modified by Roseline on 2025-07-07 01:45:37

--- TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
@@ -268,7 +268,7 @@ const NewIntents = () => {
             </div>
           </div>
         </div>
-
+        {approvedTenders.length > 0 ? (
         <div className="overflow-x-auto">
           <table className="w-full text-sm text-left text-gray-500">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50">
