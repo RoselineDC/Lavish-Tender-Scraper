6b3266b7409a2ad89a45aa0f7c35915c2c945277
RoselineDC <roseline.danga@gmail.com>
2025-07-07 01:30:16 +0200

    modified by Roseline on 2025-07-07 01:30:14

--- TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
@@ -7,6 +7,7 @@ import { TfiFilter } from "react-icons/tfi";
 import TableWithPagination from "../dashboard/TableWithPagination";
 import Link from "next/link";
 import HandleTransnetTenders from "HandleTransnetTenders";
+import { TendersPage } from "./HandleTransnetApproved";
 
 // Tailwind-compatible color map
 const colorMap: Record<string, { border: string; bg: string }> = {
