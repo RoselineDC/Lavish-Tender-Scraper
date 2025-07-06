02c211a9cd9a6c7f50ab2cfffd5fd9a54bfa960e
RoselineDC <roseline.danga@gmail.com>
2025-07-07 00:03:54 +0200

    modified by Roseline on 2025-07-07 00:03:53

--- TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
@@ -512,7 +512,7 @@ const NewIntents = () => {
         </div>
       </div>
     );
-  }
+}
   return (
     <div className="p-6">
       <h1 className="text-3xl font-bold mb-6">TRANSNET TENDERS</h1>
