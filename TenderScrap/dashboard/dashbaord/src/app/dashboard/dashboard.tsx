7b378c6f92d0e7eb6a1a3917256131ffe6d629c3
RoselineDC <roseline.danga@gmail.com>
2025-07-07 02:00:18 +0200

    modified by Roseline on 2025-07-07 02:00:17

--- TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
@@ -56,7 +56,7 @@ const Dashboard: React.FC = () => {
           footer="Pending and Paid Orders"
         />
         <Card
-          link="/tenders"
+          link="/tenders/"
           color="green"
           title="Tenders"
           subtitle="All Tenders"
