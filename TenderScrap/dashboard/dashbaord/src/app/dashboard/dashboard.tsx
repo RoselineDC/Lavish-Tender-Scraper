43f72b3c06b64f733b2c6915f3586825a774b308
RoselineDC <roseline.danga@gmail.com>
2025-07-07 02:03:32 +0200

    modified by Roseline on 2025-07-07 02:03:30

--- TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/dashboard/dashboard.tsx
@@ -20,21 +20,7 @@ const pieData = [
   { name: "Submission For Tomorrow", value: 20.69, color: "#FFC107" },
   { name: "Not Submitted", value: 16.85, color: "#ff4d4d" },
 ];
-// ✅ Fetch tenders from FastAPI on mount
-  useEffect(() => {
-    const fetchTenders = async () => {
-      try {
-        const res = await fetch("http://localhost:8000/tenders");
-        const data = await res.json();
-        setTenders(data);
-        setFilteredTenders(data);
-      } catch (error) {
-        console.error("Error fetching tenders:", error);
-      }
-    };
 
-    fetchTenders();
-  }, []);
 
 const Dashboard: React.FC = () => {
   return (
@@ -71,7 +57,7 @@ const Dashboard: React.FC = () => {
           footer="Pending and Paid Orders"
         />
         <Card
-          link="/tenders/approved"
+          link="/tenders/approve"
           color="green"
           title="Tenders"
           subtitle="All Tenders"
