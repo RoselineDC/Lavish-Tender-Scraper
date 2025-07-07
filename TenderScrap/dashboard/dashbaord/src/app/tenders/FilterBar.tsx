6bc624ec2f6a99b021fbd9c4aeccc92430b3bacf
RoselineDC <roseline.danga@gmail.com>
2025-07-07 04:05:32 +0200

    modified by Roseline on 2025-07-07 04:05:31

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -409,7 +409,7 @@ export default function FilterBar() {
                   <td className="px-4 py-3">{tender.tender_number}</td>
                   <td className="px-4 py-3">{tender.description}</td>
                   <td className="px-4 py-3">
-                    {new Date(tender.published_date).toLocaleDateString()}
+                    
                   </td>
                   <td className="px-4 py-3">{tender.closing_date}</td>
                   <td className="px-4 py-3">{tender.location}</td>
