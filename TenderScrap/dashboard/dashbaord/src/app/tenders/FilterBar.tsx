d9d00e5bd9133710f99692a984982a8aae599c67
RoselineDC <roseline.danga@gmail.com>
2025-07-10 23:07:47 +0200

    modified by Roseline on 2025-07-10 23:07:46

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -606,17 +606,14 @@ export default function FilterBar() {
                     </td>
                     <td className="px-4 py-3">{tender.tender_number}</td>
                     <td className="px-4 py-3">{tender.description}</td>
-                    <td className="px-4 py-3">{tender.published_date}</td>
                     <td className="px-4 py-3">
-                      {new Date(tender.published_date).toLocaleDateString(
-                        "en-ZA",
-                        {
-                          day: "numeric",
-                          month: "short",
-                          year: "numeric",
-                        }
-                      )}
-                    </td>
+  {new Date(tender.published_date).toLocaleDateString("en-ZA", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  })}
+</td>
+
 
                     <td className="px-4 py-3">{tender.closing_date}</td>
                     <td className="px-4 py-3">{tender.location}</td>
