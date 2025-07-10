547567e79bd49eb667b5670125a17228d8e0c335
RoselineDC <roseline.danga@gmail.com>
2025-07-10 23:13:53 +0200

    modified by Roseline on 2025-07-10 23:13:53

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -625,7 +625,17 @@ export default function FilterBar() {
                           )
                         : "N/A"}
                     </td>
-                    
+                    <td className="px-4 py-3">
+                      {typeof tender.closing_date === "string"
+                        ? parseDateString(
+                            tender.closing_date
+                          ).toLocaleDateString("en-ZA", {
+                            day: "numeric",
+                            month: "short",
+                            year: "numeric",
+                          })
+                        : "N/A"}
+                    </td>
 
                     <td className="px-4 py-3">
                       {typeof tender.closing_date === "string"
