6e861ef774a5d09d70c51124393ec4a16201e6b1
RoselineDC <roseline.danga@gmail.com>
2025-07-12 13:56:39 +0200

    modified by Roseline on 2025-07-12 13:56:38

--- TenderScrap/TenderScrap/spiders/saveToDb.py
+++ TenderScrap/TenderScrap/spiders/saveToDb.py
@@ -44,7 +44,7 @@ def parse_csv_to_db(csv_path='transnetTenders.csv', db_name='transnetTenders.db'
         for date_col in ["Published Date", "Closing Date", "Briefing Date"]:
             if date_col in df.columns:
                df[date_col] = pd.to_datetime(df[date_col], dayfirst=True, errors="coerce")
-                df[date_col] = df[date_col].dt.strftime('%Y-%m-%d').fillna("")
+            df[date_col] = df[date_col].dt.strftime('%Y-%m-%d').fillna("")
 
 
         for _, row in df.iterrows():
