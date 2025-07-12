38c25583e5c57f4c2622e159972e34b465bacb32
RoselineDC <roseline.danga@gmail.com>
2025-07-12 15:00:31 +0200

    modified by Roseline on 2025-07-12 15:00:31

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -398,7 +398,7 @@ const handleApproveTender = async (id: number | undefined) => {
                           <button
                             onClick={() => {
                               handleApproveTender(tender.id);
-                              setOpenDropdown(null);
+                              s
                             }}
                             className="block w-full px-4 py-2 hover:bg-green-100"
                           >
