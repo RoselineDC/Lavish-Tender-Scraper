c4ea0b1f2e2bda29a1765e9827dd5a2ad8210f09
RoselineDC <roseline.danga@gmail.com>
2025-07-12 13:55:35 +0200

    modified by Roseline on 2025-07-12 13:55:34

--- TenderScrap/TenderScrap/spiders/saveToDb.py
+++ TenderScrap/TenderScrap/spiders/saveToDb.py
@@ -42,8 +42,8 @@ def parse_csv_to_db(csv_path='transnetTenders.csv', db_name='transnetTenders.db'
         # Insert each row from the CSV
         # Normalize date columns dynamically
         for date_col in ["Published Date", "Closing Date", "Briefing Date"]:
-                if date_col in df.columns:
-                    df[date_col] = pd.to_datetime(df[date_col], dayfirst=True, errors="coerce").dt.strftime('%Y-%m-%d')
+            If date_col in df.columns:
+                df[date_col] = pd.to_datetime(df[date_col], dayfirst=True, errors="coerce").dt.strftime('%Y-%m-%d')
 
                     for _, row in df.iterrows():
             cursor.execute(insert_query, (
