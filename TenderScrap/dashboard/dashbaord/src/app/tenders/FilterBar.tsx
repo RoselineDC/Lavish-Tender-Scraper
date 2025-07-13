23d5e2b56275e2ceffb76491570b4721b93530fd
RoselineDC <roseline.danga@gmail.com>
2025-07-13 21:26:12 +0200

    modified by Roseline on 2025-07-13 21:26:11

--- TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/FilterBar.tsx
@@ -195,7 +195,7 @@ export default function FilterBar() {
     }
   }, [searchTerm, tenders]);
   // check tender url and document url
-console.log("Tender URL:", tenders.tender_url);
+console.log("Tender URL:", tender.tender_url);
 console.log("Tender Document URL:", tender.tender_document_url);
 
   // Toggle dropdown for each tender row actions
