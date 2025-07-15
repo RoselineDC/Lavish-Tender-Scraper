033c9e955ee7043c073011cc4ae88500de776181
RoselineDC <roseline.danga@gmail.com>
2025-07-16 00:47:25 +0200

    modified by Roseline on 2025-07-16 00:47:24

--- TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
@@ -246,6 +246,7 @@ const NewIntents = () => {
                   <th className="px-4 py-3">Closing Date</th>
                   <th className="px-4 py-3">Location</th>
                   <th className="px-4 py-3">Tender Document</th>
+                  
                   <th className="px-4 py-3">Tender Category</th>
                   <th className="px-4 py-3">Tender Status</th>
                 </tr>
