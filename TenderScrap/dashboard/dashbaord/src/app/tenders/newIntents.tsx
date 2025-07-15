1a7b9cb2972661f3869bd0e8ac28ec251f2c16ce
RoselineDC <roseline.danga@gmail.com>
2025-07-16 01:18:29 +0200

    modified by Roseline on 2025-07-16 01:18:28

--- TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
+++ TenderScrap/dashboard/dashbaord/src/app/tenders/newIntents.tsx
@@ -362,8 +362,6 @@ const NewIntents = () => {
                       className={`px-2 py-1 rounded-full text-xs font-semibold ${
                         tender.tender_status === "Open"
                           ? "bg-green-100 text-green-800"
-                          : tender.tender_status === "Approved"
-                          ? "bg-blue-100 text-blue-800"
                           : "bg-red-100 text-red-800"
                       }`}
                     >
